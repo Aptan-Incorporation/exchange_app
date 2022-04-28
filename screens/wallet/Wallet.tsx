c5cc64e4db670510f11c140d3cc5b541498d4a38
Miffy Wang <4000p049@gmail.com>
2022-04-28 11:00:27 +0800

    add button

--- screens/wallet/Wallet.tsx
+++ screens/wallet/Wallet.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Text, TextInput, TouchableOpacity, View, Image, ScrollView, SafeAreaView } from "react-native"
+import { Text, TextInput, TouchableOpacity, View, Image, ScrollView, SafeAreaView, Button } from "react-native"
 import styled from "styled-components"
 import { RootStackScreenProps } from "../../types";
 import { useState } from "react";
@@ -12,14 +12,46 @@ const Container = styled(SafeAreaView)`
     flex:1;
 `;
 
+const Row = styled(View)`
+  display:flex;
+  flex-direction: row;  
+
+
+`;
 
 const WalletScreen = ({
     navigation
 }: RootStackScreenProps<"WalletScreen">) => {
-   
+
     return (
         <Container>
-            <Text style={{color:"white"}}>123</Text>
+            <Row>
+                <Button
+                    title="總覽"
+                    color="white"
+                    onPress={() => Alert.alert('Button with adjusted color pressed')}
+                />
+
+                <Button
+                    title="現貨"
+                    color="white"
+                    onPress={() => Alert.alert('Button with adjusted color pressed')}
+                />
+
+                <Button
+                    title="合約"
+                    color="white"
+                    onPress={() => Alert.alert('Button with adjusted color pressed')}
+                />
+
+                <Button
+                    title="法幣"
+                    color="white"
+                    onPress={() => Alert.alert('Button with adjusted color pressed')}
+                />
+
+
+            </Row>
         </Container>
     )
 }
